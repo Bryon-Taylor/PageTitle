696a4f28646932ac5ee7e9bfde4757610efdac12
bryon <taylor.bryon@gmail.com>
2021-10-20 16:12:24 -0700

    display array passed from server on homepage

--- server/index.js
+++ server/index.js
@@ -1,15 +1,27 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const PORT = 3000;
 
+const urls = [];
+
 // Have Express serve static files such as CSS
 app.use(express.static('public'));
+
+// Allow parsing of body in HTTP requests
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Use EJS templates to render pages
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.render('index.ejs', {variable: " working!"});
+  res.render('index.ejs', {url: "", urls: urls});
+});
+
+app.post('/', (req, res) => {
+  console.log(req.body.inputUrl);
+  urls.push(req.body.inputUrl);
+  res.render('index.ejs', {url: req.body.inputUrl, urls: urls});
 });
 
 app.listen(PORT, () => {

8174104c4c739767a3ba3ef8894cba2cd6e989b6
bryon <taylor.bryon@gmail.com>
2021-10-20 22:15:12 -0700

    extract title and push url/title to array for display on website

--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const got = require('got');
+const request = require('request');
 const app = express();
 const PORT = 3000;
 
+let url = "";
+let title = ";"
 const urls = [];
 
 // Have Express serve static files such as CSS
@@ -15,13 +19,30 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.render('index.ejs', {url: "", urls: urls});
+  res.render('index.ejs', {url: url, title: title, urls: urls});
 });
 
 app.post('/', (req, res) => {
   console.log(req.body.inputUrl);
-  urls.push(req.body.inputUrl);
-  res.render('index.ejs', {url: req.body.inputUrl, urls: urls});
+  url = req.body.inputUrl;
+
+  (async () => {
+    try {
+      const offset = 7; // number of chars in <title>
+      const response = await got(url);
+      const sourceCode = response.body;
+      const beginIndex = sourceCode.search(/<title>/) + offset;
+      const endIndex = sourceCode.search(/<\/title>/);
+      title = await sourceCode.substring(beginIndex, endIndex);
+      console.log(title);
+      urls.push({url: url, title: title});
+      res.render('index.ejs', {url: url, title: title, urls: urls});
+    } catch (error) {
+      console.log(error.response.body);
+    }
+  })();
+
+
 });
 
 app.listen(PORT, () => {
